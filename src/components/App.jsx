00b100022f49f2b756411e505aca7a715802813e
Sergii Proiava <seregikpro@gmail.com>
2022-07-02 23:42:52 +0300

    add markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+
+export class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Please leave feedback</h1>
+        <Feedback />
+      </div>
+    );
+  }
+}
